3e81bc994dc12dbae6d7bd5e01028f1e70786479
amelia v <amelia@ag2v.com>
2025-06-18 16:26:39 -0400

    submodules, cmakelists

--- src/modules_weak/eeprom_weak.c
+++ src/modules_weak/eeprom_weak.c
@@ -3,9 +3,9 @@
 
 #define _WEAK_ __attribute__((weak))
 #define UNIMPLEMENTED (-1)
-
-_WEAK_ w_eeprom_init(void) { return UNIMPLEMENTED };
-_WEAK_ w_eeprom_full_erase(void);
-_WEAK_ w_eeprom_write(int page, uint8_t addr, uint8_t src, int size);
-_WEAK_ w_eeprom_read(int page, uint8_t addr, uint8_t *dst, int size);
-
+/*
+_WEAK_ int w_eeprom_init(void) { return UNIMPLEMENTED; };
+_WEAK_ int w_eeprom_full_erase(void){ return UNIMPLEMENTED; };
+_WEAK_ int w_eeprom_write(int page, uint8_t addr, uint8_t src, int size){ return UNIMPLEMENTED; };
+_WEAK_ int w_eeprom_read(int page, uint8_t addr, uint8_t *dst, int size){ return UNIMPLEMENTED; };
+*/
