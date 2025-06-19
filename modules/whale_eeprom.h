b6a7f18551271e803e459d0148fcf7975521ca7f
amelia v <amelia@ag2v.com>
2025-06-19 12:30:51 -0400

    finish whale eeprom funcs

--- modules/whale_eeprom.h
+++ modules/whale_eeprom.h
@@ -6,17 +6,37 @@
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
 
-#define EEPROM_OK 0
-#define EEPROM_ERR 1
-#define EEPROM_BUSY 2
+#define W_EEPROM_OK 0
+#define W_EEPROM_ERR 1
 
-#ifndef EEPROM_A1
-#define EEPROM_A1 (0)
+#ifndef W_EEPROM_SEL
+#define W_EEPROM_SEL (0x00)
 #endif
 
-#ifndef EEPROM_A2
-#define EEPROM_A2 (0)
-#endif
+#define W_SCL_PIN (5)
+#define W_SDA_PIN (4)
+#define W_I2C_INST (i2c0)
+
+/*
+ * using this library:
+ * the read/write functions work with strings / uint8_t arrays of up to size 256
+ * they will return an error if you try to write to a page or address out of bounds
+ * or if you try to write more than 256 bytes at a time
+ *
+ * in your main program, #define W_EEPROM_SEL to be equal to the A2 and A1 bits
+ * which are set physically with resistors. this allows a range of 0x00 to 0x03
+ *
+ * example:
+ * 	w_eeprom_write(420, 0x45, ex_src_string, sizeof(ex_src_string));
+ * 	w_eeprom_read(420, 0x45, ex_dst_string, sizeof(ex_dst_string));
+ *
+ * to read a single byte make sure to either use an array of size 1
+ * or explicitly pass it a pointer!
+ *
+ * all functions return either W_EEPROM_OK or W_EEPROM_ERR
+ */
+
+
 
 int w_eeprom_init(void);
 
@@ -24,5 +44,7 @@ int w_eeprom_full_erase(void);
 
 int w_eeprom_write(int page, uint8_t addr, uint8_t *src, int size);
 int w_eeprom_read(int page, uint8_t addr, uint8_t *dst, int size);
+
+int w_eeprom_is_busy_timeout(uint ms);
 
 #endif
